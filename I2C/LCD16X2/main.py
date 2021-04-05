f05b577a507a5b0f4ebdd63b7cd04ec342236ad3
Arun <75924781+controllerstech@users.noreply.github.com>
2021-04-05 12:49:55 +0530

    Update main.py

--- I2C/LCD16X2/main.py
+++ I2C/LCD16X2/main.py
@@ -8,5 +8,17 @@ def main():
     i2clcd.goto(1,1)
     i2clcd.string('from PI PICO')
     
+    value = 12345
+    flot = 123.45
+    
+    i2clcd.clear()
+    i2clcd.goto(0,1)
+    i2clcd.string(str(value))
+    i2clcd.goto(1,1)
+    i2clcd.string(str(flot))
+    
+    
+    
+    
 if __name__ == "__main__":
     main()
